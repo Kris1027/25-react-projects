d3239bb5b70bf3afb953bd45e7d61ad76ea506ab
Kris1027 <obarzanek.work@gmail.com>
2024-03-27 14:37:54 +0100

    add functions for next and prev image in image-slider

--- src/projects/image-slider/ImageSlider.tsx
+++ src/projects/image-slider/ImageSlider.tsx
@@ -1,5 +1,81 @@
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { FaArrowAltCircleRight } from "react-icons/fa";
+
 export const ImageSlider: React.FC = () => {
+  const [activeImage, setActiveImage] = useState<number>(1);
+
+  const handlePrevImage = () => {
+    if (activeImage === 1) {
+      setActiveImage(5);
+    } else {
+      setActiveImage(activeImage - 1);
+    }
+  };
+  const handleNextImage = () => {
+    if (activeImage === 5) {
+      setActiveImage(1);
+    } else {
+      setActiveImage(activeImage + 1);
+    }
+  };
+
   return (
-    <div>ImageSlider</div>
-  )
-}
+    <div className="p-4">
+      <div className="flex items-center pb-4">
+        <FaArrowAltCircleLeft
+          size={40}
+          className="cursor-pointer active:scale-95"
+          onClick={handlePrevImage}
+        />
+        <img
+          className="w-96 h-96"
+          src={`/src/assets/image-slider/carousel-${activeImage}.jpg`}
+        />
+        <FaArrowAltCircleRight
+          size={40}
+          className="cursor-pointer active:scale-95"
+          onClick={handleNextImage}
+        />
+      </div>
+      <div className="flex gap-2 justify-center">
+        <button
+          className={twMerge(
+            "w-4 h-4 bg-black rounded-full",
+            activeImage === 1 && "bg-red-500"
+          )}
+          onClick={() => setActiveImage(1)}
+        ></button>
+        <button
+          className={twMerge(
+            "w-4 h-4 bg-black rounded-full",
+            activeImage === 2 && "bg-red-500"
+          )}
+          onClick={() => setActiveImage(2)}
+        ></button>
+        <button
+          className={twMerge(
+            "w-4 h-4 bg-black rounded-full",
+            activeImage === 3 && "bg-red-500"
+          )}
+          onClick={() => setActiveImage(3)}
+        ></button>
+        <button
+          className={twMerge(
+            "w-4 h-4 bg-black rounded-full",
+            activeImage === 4 && "bg-red-500"
+          )}
+          onClick={() => setActiveImage(4)}
+        ></button>
+        <button
+          className={twMerge(
+            "w-4 h-4 bg-black rounded-full",
+            activeImage === 5 && "bg-red-500"
+          )}
+          onClick={() => setActiveImage(5)}
+        ></button>
+      </div>
+    </div>
+  );
+};
