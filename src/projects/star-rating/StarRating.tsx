1050a61111f060a93ae617c93ad00d0a93f8141b
Kris1027 <obarzanek.work@gmail.com>
2024-03-27 12:17:56 +0100

    build functions for rating and hover of stars

--- src/projects/star-rating/StarRating.tsx
+++ src/projects/star-rating/StarRating.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
 export const StarRating = () => {
-  return <div>{<FaStar />}</div>;
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  const numberOfStars = 10;
+  const size = 40;
+
+  return (
+    <div className="flex p-4">
+      {[...Array(numberOfStars)].map((_, index) => {
+        index += 1;
+        return (
+          <FaStar
+            key={index}
+            size={size}
+            onClick={() => setRating(index)}
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(rating)}
+            color={index <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+          />
+        );
+      })}
+    </div>
+  );
 };
