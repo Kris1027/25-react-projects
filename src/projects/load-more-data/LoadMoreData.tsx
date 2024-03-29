ddcb2244e8085ab2ac1d99d9b3838476923bee75
Kris1027 <obarzanek.work@gmail.com>
2024-03-29 12:28:17 +0100

    fix fetching bugs

--- src/projects/load-more-data/LoadMoreData.tsx
+++ src/projects/load-more-data/LoadMoreData.tsx
@@ -12,29 +12,29 @@ export const LoadMoreData = () => {
   const [count, setCount] = useState<number>(0);
   const [disableButton, setDisableButton] = useState<boolean>(false);
 
-  const fetchProducts = async () => {
-    try {
-      setIsLoading(true);
-      const response = await fetch(
-        `https://dummyjson.com/products?limit=20&skip=${
-          count === 0 ? 0 : count * 20
-        }`
-      );
-      const result = await response.json();
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `https://dummyjson.com/products?limit=20&skip=${
+            count === 0 ? 0 : count * 20
+          }`
+        );
+        const result = await response.json();
 
-      if (result && result.products && result.products.length) {
-        setProducts((prevData) => [...prevData, ...result.products]);
+        if (result && result.products && result.products.length) {
+          setProducts((prevData) => [...prevData, ...result.products]);
+          setIsLoading(false);
+
+          console.log(result);
+        }
+      } catch (error) {
+        console.error(error);
         setIsLoading(false);
       }
+    };
 
-      console.log(result);
-    } catch (error) {
-      console.log(error);
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
     fetchProducts();
   }, [count]);
 
@@ -46,29 +46,24 @@ export const LoadMoreData = () => {
 
   return (
     <div className="p-4">
-      <div className="flex flex-wrap justify-center gap-4">
-        {products && products.length
-          ? products.map((product: ProductProps) => (
-              <div
-                className="border-2 border-black w-1/6 h-auto"
-                key={product.id}
-              >
-                <img
-                  className="object-cover"
-                  src={product.thumbnail}
-                  alt={product.title}
-                />
-                <p className="p-2 text-center">{product.title}</p>
-              </div>
-            ))
-          : null}
+      <div className="grid grid-cols-10 gap-4">
+        {products.map((product: ProductProps) => (
+          <div className="border-2 border-black" key={product.id}>
+            <img
+              className="object-cover"
+              src={product.thumbnail}
+              alt={product.title}
+            />
+            <p className="p-2 text-center">{product.title}</p>
+          </div>
+        ))}
       </div>
       <div className="flex justify-center p-10">
         {disableButton ? (
           <p>All Products Loaded</p>
         ) : (
           <button
-            onClick={() => setCount(count + 1)}
+            onClick={() => setCount((prevCount) => prevCount + 1)}
             className="py-4 px-8 bg-slate-400 text-slate-950 rounded-lg hover:bg-slate-300 active:scale-90"
           >
             Load More Products
