51a29b539b58b8d831a05708251cda25fbd16bd6
Kris1027 <obarzanek.work@gmail.com>
2024-04-01 10:51:54 +0200

    add styles

--- src/projects/qr-code-generator/QrCodeGenerator.tsx
+++ src/projects/qr-code-generator/QrCodeGenerator.tsx
@@ -12,9 +12,10 @@ export const QrCodeGenerator = () => {
 
   return (
     <div>
-      <h1>QR Code Generator</h1>
-      <div>
+      <h1 className="text-3xl text-center pb-4">QR Code Generator</h1>
+      <div className="flex flex-col">
         <input
+          className="px-4 py-2 bg-slate-200 rounded-lg mb-4"
           type="text"
           name="qr-code"
           placeholder="Enter your value here"
@@ -22,13 +23,14 @@ export const QrCodeGenerator = () => {
           onChange={(e) => setInputValue(e.target.value)}
         />
         <button
+          className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 active:scale-90"
           disabled={inputValue && inputValue.trim() !== "" ? false : true}
           onClick={handleGenerateQrCode}
         >
           Generate
         </button>
       </div>
-      <div>
+      <div className="pt-4">
         <QRCode id="qr-code-value" value={qrCode} size={400} bgColor="#fff" />
       </div>
     </div>
