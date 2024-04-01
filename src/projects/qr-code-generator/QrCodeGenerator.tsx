15f91eab18da59efe9a1883ead8a81159d76a6b2
Kris1027 <obarzanek.work@gmail.com>
2024-04-01 10:47:09 +0200

    implement qr-code-generator functions

--- src/projects/qr-code-generator/QrCodeGenerator.tsx
+++ src/projects/qr-code-generator/QrCodeGenerator.tsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+import QRCode from "react-qr-code";
+
 export const QrCodeGenerator = () => {
-  return <div>QrCodeGenerator</div>;
+  const [qrCode, setQrCode] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  function handleGenerateQrCode() {
+    setQrCode(inputValue);
+    setInputValue("");
+  }
+
+  return (
+    <div>
+      <h1>QR Code Generator</h1>
+      <div>
+        <input
+          type="text"
+          name="qr-code"
+          placeholder="Enter your value here"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button
+          disabled={inputValue && inputValue.trim() !== "" ? false : true}
+          onClick={handleGenerateQrCode}
+        >
+          Generate
+        </button>
+      </div>
+      <div>
+        <QRCode id="qr-code-value" value={qrCode} size={400} bgColor="#fff" />
+      </div>
+    </div>
+  );
 };
